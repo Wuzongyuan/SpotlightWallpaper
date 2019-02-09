f505c55da53c72a13e5359c3a7e1cf53d9b6380c
null <wzy199498@foxmail.com>
2019-02-09 10:11:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# win10-spotlight
+获取win10聚焦壁纸
+
+##### 1.运行项目
+
+```
+node index
+```
+
+
+
+运行后在图片目录下会有spotlight目录
+该目录下是win10的聚焦壁纸，包括手机和pc的。
+
+##### 2.另外还可以点击 run.vbs 来运行
+
+##### 3.借鉴了git@github.com:guobing1993/win10-spotlight.git
---
+++ index.js
@@ -0,0 +1,76 @@
+const fs = require('mz/fs');
+const path = require('path');
+const imageInfo = require('image-info');
+
+/*壁纸所在的源文件夹*/
+const src = path.join(process.env.localappdata, "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\");
+/*要拷贝到的文件夹：这个是在‘图片/spotlight’*/
+const target = path.join('D:\\SpotlightWallpaper');
+
+start();
+
+async function start() {
+	await checkdir();
+	await copyFile();
+}
+
+/*判断spotlight文件夹是否存在*/
+async function checkdir() {
+	try {
+		const s = await fs.stat(target);
+		if (!s.isDirectory()) {
+			await fs.mkdir(target);
+		}
+	} catch (error) {
+		await fs.mkdir(target);
+	}
+}
+
+/*读取源文件目录下的所有文件*/
+async function copyFile() {
+	const files = await fs.readdir(src);
+	files.forEach(async file => {
+		const stats = await fs.stat(path.join(src, file));
+		if (stats.isFile()) {
+			const data = await fs.readFile(path.join(src, file));
+			let filename;
+			const tempData = data.slice(0, 7);
+			if (tempData.indexOf(Buffer.from('FFD8FF', "hex")) != -1) {
+
+				filename = path.join(target, file + ".jpg");
+
+			} else if (tempData.indexOf(Buffer.from('89504E47', 'hex')) != -1) {
+
+				filename = path.join(target, file + ".png");
+			}
+			if (filename) {
+				const info = await imgInfo(path.join(src, file));
+				var wh = info.height / info.width;
+				if (wh != 1 && info.bytes > 102400 && info.width > info.height) {
+					await fs.writeFile(filename, data, function (err, data) {
+						if (err) {
+							console.error(err);
+							console.error("update fail:" + filename);
+						} else {
+							console.log("update success:" + filename);
+						}
+					})
+				}
+			}
+		}
+
+	});
+}
+
+
+function imgInfo(filename) {
+	return new Promise(function (resolve, reject) {
+		imageInfo(filename, (err, info) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(info);
+
+		})
+	})
+}
